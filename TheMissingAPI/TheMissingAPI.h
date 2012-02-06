a11b0258d2e192a537b757e3b0eafc5ba1d1809a
Oliver Drobnik <oliver@drobnik.com>
2012-02-06 08:16:27 +0100

    Initial Commit

---
+++ TheMissingAPI/TheMissingAPI.h
@@ -0,0 +1,13 @@
+//
+//  TheMissingAPI.h
+//  TheMissingAPI
+//
+//  Created by Oliver Drobnik on 06.02.12.
+//  Copyright (c) 2012 Drobnik KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TheMissingAPI : NSObject
+
+@end
---
+++ TheMissingAPI/TheMissingAPI.m
@@ -0,0 +1,23 @@
+//
+//  TheMissingAPI.m
+//  TheMissingAPI
+//
+//  Created by Oliver Drobnik on 06.02.12.
+//  Copyright (c) 2012 Drobnik KG. All rights reserved.
+//
+
+#import "TheMissingAPI.h"
+
+@implementation TheMissingAPI
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
